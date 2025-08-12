ea5c497024ce6177b58e270bfb3fb73155a7d432
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2025-08-12 15:33:57 +0200

    refactor: enhance Astro configuration with sidebar and locales support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,27 +7,36 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dolmen-tools.github.io/angular-tutorials',
-    base: '/angular-tutorials/',
-  integrations: [starlight({
-    title: "Docs with Tailwind",
-    social: [
-      {
-        icon: "github",
-        label: "GitHub",
-        href: "https://github.com/withastro/starlight",
+  site: "https://dolmen-tools.github.io/angular-tutorials",
+  base: "/angular-tutorials/",
+  integrations: [
+    starlight({
+      title: "Docs with Tailwind",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Getting Started",
+          link: "/angular-tutorials/getting-started",
+        },
+      ],
+      customCss: ["./src/styles/global.css"],
+      // Set English as the default language for this site.
+      defaultLocale: "en",
+      locales: {
+        // English docs in `src/content/docs/en/`
+        en: {
+          label: "English",
+        },
       },
-    ],
-    customCss: ["./src/styles/global.css"],
-    // Set English as the default language for this site.
-    defaultLocale: "en",
-    locales: {
-      // English docs in `src/content/docs/en/`
-      en: {
-        label: "English",
-      },
-    },
-  }), react()],
+    }),
+    react(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
